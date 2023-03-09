3dee21dedca148ddd39a15be41948b49b85b31e1
Cristhina Vargas <usuario@usuarios-MacBook-Pro.local>
2023-03-09 10:40:33 -0500

    first commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,187 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //IBAaction for bottons from xylophone
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    @IBAction func keyPressed2(_ sender: UIButton) {
+        letterD()
+    }
+    
+    @IBAction func keyPressedE(_ sender: UIButton) {
+        letterE()
+    }
+    @IBAction func keyPressedF(_ sender: UIButton) {
+        letterF()
+    }
+    @IBAction func keyPressedG(_ sender: UIButton) {
+        letterG()
+    }
+    
+    @IBAction func keyPressedA(_ sender: UIButton) {
+        letterA()
+    }
+    
+    @IBAction func keyPressedB(_ sender: UIButton) {
+        letterB()
+    }
+    //making sound
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    //funcion to letter D
+    func letterD() {
+        guard let url = Bundle.main.url(forResource: "D", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
+    //fuction to letter E
+    func letterE () {
+        guard let url = Bundle.main.url(forResource: "E", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    //For letter F
+    func letterF () {
+        guard let url = Bundle.main.url(forResource: "F", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    // for G
+    func letterG(){
+        guard let url = Bundle.main.url(forResource: "G", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    // for A
+    func letterA(){
+        guard let url = Bundle.main.url(forResource: "A", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    // for B
+    func letterB(){
+        guard let url = Bundle.main.url(forResource: "B", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+}
+
