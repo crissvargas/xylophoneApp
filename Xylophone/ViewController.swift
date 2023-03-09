925de1c5e97d9a73759e589b0444c9e277f19686
Cristhina Vargas <usuario@usuarios-MacBook-Pro.local>
2023-03-09 10:57:47 -0500

    adding new fuction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -41,6 +41,9 @@ class ViewController: UIViewController {
     @IBAction func keyPressedB(_ sender: UIButton) {
         letterB()
     }
+    @IBAction func keyPressedB(_ sender: UIButton) {
+        letterZ()
+    }
     //making sound
     func playSound() {
         guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
@@ -165,6 +168,26 @@ class ViewController: UIViewController {
     // for B
     func letterB(){
         guard let url = Bundle.main.url(forResource: "B", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+        
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    //for letter Z (example of brach)
+    func letterZ(){
+        guard let url = Bundle.main.url(forResource: "Z", withExtension: "wav") else { return }
 
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
